b16890c42f2fee735a4df6660d201557db604d98
msgirlperl <mspeert@gmail.com>
2018-07-22 18:45:50 -0400

    changed mime type of docs to pdf

--- routes/documents.js
+++ routes/documents.js
@@ -1,7 +1,7 @@
-const keys = require('../config/keys')
-require('isomorphic-fetch') // or another library of choice.
-const Dropbox = require('dropbox').Dropbox
-const dbx = new Dropbox({accessToken: keys.dropboxAccessToken})
+const keys = require('../config/keys');
+require('isomorphic-fetch'); // or another library of choice.
+const Dropbox = require('dropbox').Dropbox;
+const dbx = new Dropbox({ accessToken: keys.dropboxAccessToken });
 
 module.exports = app => {
   // app.get(
@@ -20,25 +20,26 @@ module.exports = app => {
 
   app.get('/api/document_list', (req, res) => {
     dbx
-      .filesListFolder({path: '/hearing loss'})
-      .then(docs => res.json(docs.entries)) 
+      .filesListFolder({ path: '/hearing loss' })
+      .then(docs => res.json(docs.entries))
       .catch(function(error) {
-        res.json({errorMessage: error})
-      })
-  })
-
-  app.get('/api/document/:path*', (req, res) => {
-
-    // want the entire path param but the '/' have been custom-encoded with ~2F
-    let downloadPath = '/' + decodeURIComponent(req.params.path).replace('~2F', '/')
-
-    dbx.filesDownload({path: downloadPath})
-     .then(doc => {
-       res.send(new Buffer(doc.fileBinary, 'binary'));
-     })
-    .catch(function(error) {
-      res.json({errorMessage: error})
-    })
+        res.json({ errorMessage: error });
+      });
   });
 
-}
+  app.get('/api/document/:path*', (req, res) => {
+    // want the entire path param but the '/' have been custom-encoded with ~2F
+    let downloadPath =
+      '/' + decodeURIComponent(req.params.path).replace('~2F', '/');
+
+    dbx
+      .filesDownload({ path: downloadPath })
+      .then(doc => {
+        res.header('Content-Type', 'application/pdf');
+        res.send(new Buffer(doc.fileBinary, 'binary'));
+      })
+      .catch(function(error) {
+        res.json({ errorMessage: error });
+      });
+  });
+};
