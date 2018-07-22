cb9b0f4a023efd5604ddee1dc6c70ac8eef91461
msgirlperl <mspeert@gmail.com>
2018-07-22 18:18:00 -0400

    trying to fix links not opening right

--- client/src/components/file.js
+++ client/src/components/file.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 // export default class File extends Component {
 //   render() {
@@ -6,10 +6,10 @@ import React from 'react'
 //   }
 // }
 
-const File = ({id, name, path}) => {
+const File = ({ id, name, path }) => {
   // express routing will convert / to the end of the param but we want the entire path as the param
-  const filePath = path.substr(1).replace('/', '~2F')
-  const url = `/api/document/${filePath}`
+  const filePath = path.substr(1).replace('/', '~2F');
+  const url = `/api/document/${filePath}`;
   return (
     // <li className="list-group-item">
     //   <a target="_blank" href={url}>{name}</a>
@@ -21,7 +21,7 @@ const File = ({id, name, path}) => {
         </a>
       </td>
     </tr>
-  )
-}
+  );
+};
 
-export default File
+export default File;
